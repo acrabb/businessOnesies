296cd1ac5dd160e2f58e40b2014a33279a428ce1
Andre Crabb <crabb.andre@gmail.com>
2013-04-01 12:24:51 -0700

    added file for UI elements popup

---
+++ src/edu/berkeley/cs160/onesies/metaapp/UIPopup.java
@@ -0,0 +1,11 @@
+package edu.berkeley.cs160.onesies.metaapp;
+
+import android.app.Activity;
+
+public class UIPopup extends Activity {
+
+	public UIPopup() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
