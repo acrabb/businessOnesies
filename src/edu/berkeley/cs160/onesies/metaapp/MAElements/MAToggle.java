471572a8a61776720d932b3b0c6866dec16b1ade
Achal <achalddave@live.com>
2013-05-01 23:35:14 -0700

    #9: toggle button implemented

--- src/edu/berkeley/cs160/onesies/metaapp/MAElements/MAToggle.java
+++ src/edu/berkeley/cs160/onesies/metaapp/MAElements/MAToggle.java
@@ -5,10 +5,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
-import android.graphics.RectF;
+import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.RelativeLayout;
 import edu.berkeley.cs160.onesies.metaapp.ElementType;
 import edu.berkeley.cs160.onesies.metaapp.MAScreen;
@@ -21,13 +20,14 @@ public class MAToggle extends MAScreenElement {
 	private Paint       paint;
 	
 	private int MIN_WIDTH = 60;
-	private int	MIN_HEIGHT = 40;
+	private int	MIN_HEIGHT = 70;
 	private int	MAX_HEIGHT = MIN_HEIGHT;
 	private int padding = 10;
 	private int textSize = 20;
-	private int minToggleWidth = 5;
-	private RectF mRectF;
 	private boolean toggleOn = false;
+	private Rect textBounds;
+	private String on = "On";
+	private String off = "Off";
 	
 	public MAToggle(Context context, MAScreen maScreen) {
 		super(context, maScreen, ElementType.CHECKBOX);
@@ -36,12 +36,12 @@ public class MAToggle extends MAScreenElement {
 		paint = new Paint();
 		paint.setDither(true);
 		paint.setTextSize(textSize);
-		paint.setTextAlign(Paint.Align.LEFT);
+		paint.setTextAlign(Paint.Align.CENTER);
 		paint.setTypeface(Typeface.DEFAULT_BOLD);
 		paint.setColor(Color.BLACK);
 		mText = "Toggle";
+		textBounds = new Rect();
 		// set right and bottom in onDraw
-		mRectF = new RectF(padding, padding, 0, 0);
 	}
 
 	public MAToggle(Context context, AttributeSet attrs) {
@@ -52,38 +52,42 @@ public class MAToggle extends MAScreenElement {
 	@Override
 	public void onDraw(Canvas canvas) {
 		float h = this.getHeight();
-		float toggleWidth = (Math.max(minToggleWidth, Math.min(this.getWidth(), 2*h)));
+		float w = this.getWidth();
+		float toggleHeight = h - 2*padding;
+		float toggleWidth  = w - 2*padding;
+
 		float l = padding;
-		float t = padding;
 		float r = l + toggleWidth;
-		float b = h - padding;
-
-		paint.setColor(Color.BLACK);
-		paint.setStyle(Style.STROKE);
-		paint.setStrokeWidth(h / 10);
-
-		paint.setStyle(Style.STROKE);
+		float t = padding;
+		float b = t + toggleHeight;
+		
+		paint.setStyle(Style.FILL);
+		paint.setColor(getResources().getColor(R.color.uiGray));
 		canvas.drawRect(l, t, r, b, paint);
 
+		paint.setStrokeWidth(10);
+		paint.setStyle(Style.STROKE);
+		canvas.drawRect(l, t, r, b, paint);
+		
+		paint.setColor(Color.BLACK);
 		if (toggleOn) {
-			paint.setStrokeWidth(0);
+			canvas.drawRect(l + (toggleWidth / 2), t, r, b, paint);
 			paint.setStyle(Style.FILL);
-			paint.setColor(Color.BLACK);
-			canvas.drawRect(l, t, l + (toggleWidth / 2), b, paint);
-
-			paint.setStyle(Style.FILL);
-			paint.setColor(getResources().getColor(R.color.uiGray));
-			canvas.drawRect(l + (toggleWidth / 2), t, l + toggleWidth, b, paint);
+			paint.setColor(getResources().getColor(R.color.uiDarkerGray));
+			canvas.drawRect(l + (toggleWidth / 2), t, r, b, paint);
 		} else {
-			paint.setStrokeWidth(0);
-			paint.setStyle(Style.FILL);
-			paint.setColor(getResources().getColor(R.color.uiGray));
 			canvas.drawRect(l, t, l + (toggleWidth / 2), b, paint);
-
 			paint.setStyle(Style.FILL);
-			paint.setColor(Color.BLACK);
-			canvas.drawRect(l + (toggleWidth / 2), t, l + toggleWidth, b, paint);
+			paint.setColor(getResources().getColor(R.color.uiDarkerGray));
+			canvas.drawRect(l, t, l + (toggleWidth / 2), b, paint);
 		}
+		
+		paint.setColor(Color.BLACK);
+		paint.getTextBounds(off, 0, off.length(), textBounds);
+		canvas.drawText(off, l + (toggleWidth / 4), h / 2 - (textBounds.top - textBounds.bottom)/2, paint);
+		
+		paint.getTextBounds(on, 0, on.length(), textBounds);
+		canvas.drawText(on, r - (toggleWidth / 4), h / 2 - (textBounds.top - textBounds.bottom)/2, paint);
 	}
 	
 	//-----------------GETTERS AND SETTERS-----------------------------------
